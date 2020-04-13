a78e28ea9f0a601f7d0659cde1c39c7d2a57eab7
Kwan Noel <noelkwan1998@gmail.com>
2020-04-13 12:13:38 +0800

    Remove write-to-file function calls from test spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,18 +1,13 @@
 import Test.Hspec
 import Test.Hspec.Core.Spec (sequential)
 
-import Servant.Docs.Simple (writeDocsJson, writeDocsPlainText)
 import Test.Servant.Docs.Simple (mainSpec)
 import Test.Servant.Docs.Simple.Parse (parseSpec)
 import Test.Servant.Docs.Simple.Render (renderSpec)
-import Test.Servant.Docs.Simple.Samples (ApiMultiple)
 
 main :: IO ()
 main = do
-  writeDocsPlainText @ApiMultiple "documentation"
-  writeDocsJson @ApiMultiple "documentationJson"
-
-  hspec $ sequential $ do
-    renderSpec
-    parseSpec
-    mainSpec
+    hspec $ sequential $ do
+        renderSpec
+        parseSpec
+        mainSpec
